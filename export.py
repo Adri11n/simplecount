f79c726aadfcc6d9130c92e6cdac4327b6d42397
Adri11n <adriaanvanvliet2005@gmail.com>
2021-03-07 19:40:09 +0100

    added logging email feature

--- export.py
+++ export.py
@@ -1,22 +1,35 @@
 import sys
 import smtplib
 import ssl
+which = sys.argv[1]
+counter = sys.argv[2]
+smtp_server = sys.argv[3]
+sender_email = sys.argv[4]
+port = int(sys.argv[5])
+receiver_email = sys.argv[6]
+password = sys.argv[7]
 
-counter = sys.argv[1]
-smtp_server = sys.argv[2]
-sender_email = sys.argv[3]
-port = int(sys.argv[4])
-receiver_email = sys.argv[5]
-password = sys.argv[6]
-
-with open(counter + ".txt", "r") as database:
-    value = database.read()
-message = f"""\
-Subject: Here is the exported value of you simplecount counter {counter}
+if which == "value":
+    with open(counter + ".txt", "r") as database:
+        value = database.read()
+    message = f"""\
+Subject: Here is the exported value of your simplecount counter {counter}
 
 The value of your counter is {value}."""
 
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message)
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
+elif which == "log":
+    with open("simplecount.log", "r") as database:
+        value = database.read()
+    message = f"""\
+Subject: Here is the exported log of your simplecount log
+
+{value}."""
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
